abfb7dc7f310fbf57fd6eb4af6d5e00e09796bcf
julhariemaddin-dev <julharie.maddin.13@gmail.com>
2026-08-07 20:59:52 +0800

    refactor 2

--- src/main/java/com/seams/backend/core/repository/ProblemRequestRepository.java
+++ src/main/java/com/seams/backend/core/repository/ProblemRequestRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface ProblemRequestRepository extends JpaRepository<ProblemRequest, Integer> {
